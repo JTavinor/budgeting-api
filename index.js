cb7f40c65677198844163aa26e093fb352e02d3b
Joe Tavinor <joe.tavinor@appscore.net>
2023-09-26 21:23:43 +0100

    add group type and resolver

--- index.js
+++ index.js
@@ -8,27 +8,24 @@ const typeDefs = `#graphql
   # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
 
   # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: String
+  type Group {
+    groupName: String
   }
 
   # The "Query" type is special: it lists all of the available queries that
   # clients can execute, along with the return type for each. In this
   # case, the "books" query returns an array of zero or more Books (defined above).
   type Query {
-    books: [Book]
+    groups: [Group]
   }
 `;
 
-const books = [
+const groups = [
   {
-    title: "The Awakening",
-    author: "Kate Chopin",
+    groupName: "Car",
   },
   {
-    title: "City of Glass",
-    author: "Paul Auster",
+    groupName: "Misc",
   },
 ];
 
@@ -36,7 +33,7 @@ const books = [
 // This resolver retrieves books from the "books" array above.
 const resolvers = {
   Query: {
-    books: () => books,
+    groups: () => groups,
   },
 };
 
